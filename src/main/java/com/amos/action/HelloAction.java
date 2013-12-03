c3d582fa0a100b428d2f0fb9bd07612b7dacec2e
liyayun <amosli@infomorrow.com>
2013-12-04 01:41:09 +0800

    struts2 && spring integrated is first test ok

---
+++ src/main/java/com/amos/action/HelloAction.java
@@ -0,0 +1,22 @@
+package com.amos.action;
+
+import org.apache.struts2.ServletActionContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.amos.service.IHello;
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionContext;
+
+public class HelloAction {
+	private IHello hello;
+	public String execute(){
+		ApplicationContext acx = WebApplicationContextUtils.getWebApplicationContext(ServletActionContext.getServletContext());
+		hello = acx.getBean(IHello.class);
+		//调用业务层组件
+		String msg = hello.sayHi();
+		System.out.println("msg:"+msg);
+		ActionContext.getContext().put("msg", msg);
+		return Action.SUCCESS;
+	}
+}
